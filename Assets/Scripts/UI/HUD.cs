c1cf418ea49d40cfaeb2626174805687c7066ed3
LOST-DLL <3103736510@qq.com>
2026-02-01 10:14:06 +0800

    update: HUD与商店界面

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -10,6 +10,18 @@ public class HUD : MonoBehaviour {
     [SerializeField] private Text Combo;
     [SerializeField] private Button PauseButton;
     [SerializeField] private GameObject Panel;
+    [SerializeField] private Button BuyBonusLevel;
+    [SerializeField] private Button BuyRateLevel;
+    [SerializeField] private Button BuySpLevel;
+    [SerializeField] private Button BuySpecialBonusLevel;
+    [SerializeField] private Text BonusPrice;
+    [SerializeField] private Text RatePrice;
+    [SerializeField] private Text SpPrice;
+    [SerializeField] private Text SpecialBonusPrice;
+    [SerializeField] private Text BonusLevel;
+    [SerializeField] private Text RateLevel;
+    [SerializeField] private Text SpLevel;
+    [SerializeField] private Text SpecialBonusLevel;
 
     // Start is called before the first frame update
     void Start() {
@@ -30,11 +42,27 @@ public class HUD : MonoBehaviour {
         };
 
         GameManager.Instance.OnMoneyUpdated += (int money) => {
-            CoinsCount.text = money.ToString();
+            CoinsCount.text = "гд" + money.ToString();
         };
 
         PauseButton.onClick.AddListener(() => {
             Panel.SetActive(!Panel.activeInHierarchy);
+        });
+
+        BuyBonusLevel.onClick.AddListener(() => {
+
+        });
+
+        BuyRateLevel.onClick.AddListener(() => {
+
+        });
+
+        BuySpLevel.onClick.AddListener(() => {
+
+        });
+
+        BuySpecialBonusLevel.onClick.AddListener(() => {
+
         });
     }
 
